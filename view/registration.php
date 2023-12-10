744c9f3774b5f572611c55052a98879de8c7b1a5
mdtanvirisl <mdtanvirislam945@gmail.com>
2023-12-10 12:21:48 +0600

    css changes

--- view/registration.php
+++ view/registration.php
@@ -6,79 +6,76 @@ include_once("../controller/registrationCheck.php");
 
 <head>
     <title>Registration</title>
+    <link rel="stylesheet" href="../assets/css/body.css">
 </head>
 
 <body>
     <?php include_once("home_menu.php");?>
-    <table border = '1' width= 100%>
-        <tr>
-            <td colspan = "2">
-                <center>
-                    <form action="" method="POST" enctype="">
+        <form action="" method="POST" enctype="">
+            <h3 class="page_title">REGISTRATION</h3>
+            <hr>
+            <div class="form">
+            <fieldset>                
+                <div class="input_box">
+                    <label for="name"><b>Name: </b></label>
+                    <input type="text" id="name" name="name" value="<?php echo $name ?>"><span id="nameErr"></span>  
+                    <?= $nameError ?> <br> <br>
 
+                    <label for="number"><b>Number: </b></label> </td>
+                    <input type="text" id="number" name="number" value="<?php echo $number ?>" ><span id="numberErr"></span> 
+                    <?= $numberError ?> <br> <br> 
 
-                        <h3>REGISTRATION</h3>
-                        <hr>
-                        <table border="0" cellspacing="0" cellpadding="0">
+                    <label for="gender"><b>Gender: </b></label> 
+                        <input type="radio" id="gender" name="gender" value="Male" <?= ($gender && $gender == "Male") ?
+                        'checked="checked"' : ""; 
+                        ?> /> Male
+                        <input type="radio" id="gender" name="gender" value="Female" <?= ($gender && $gender == "Female") ?
+                        'checked="checked"' : "";
+                        ?> /> Female
+                        <input type="radio" id="gender" name="gender" value="Other" <?= ($gender && $gender == "Other") ?
+                        'checked="checked"' : "";
+                        ?> /> Other
 
+                    <span id="genderErr"></span><?= $genderError ?> <br> <br>
 
-                            <tr>
-                                <td><label for="name">Name</label> </td>
-                                <td> : <input type="text" name="name" value="<?php echo $name ?>"/> <span><?= $nameError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="username">Username</label> </td>
-                                <td> : <input type="text" name="username" value="<?php echo $username ?>"/> <span><?= $usernameError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="email">Email</label> </td>
-                                <td> : <input type="email" name="email" value="<?php echo $email ?>"/> <span><?= $emailError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="number">Number</label> </td>
-                                <td> : <input type="text" name="number" value="<?php echo $number ?>"/> <span><?= $numberError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="password">Password</label> </td>
-                                <td> : <input type="text" name="password" value="<?php echo $password ?>"/> <span><?= $passwordError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="confirmPassword">Confirm Password</label> </td>
-                                <td> : <input type="text" name="confirmPassword" value="<?php echo $confirmPassword ?>"/> <span><?= $confirmPasswordError ?></span> </td>
-                            </tr>
-                            <tr>
-                                <td><label for="gender">Gender</label> </td>
-                                <td> :
-                                    <input type="radio" name="gender" value="Male" <?= ($gender && $gender == "Male") ?
-                                                                                        'checked="checked"' : "";
-                                                                                    ?> /> Male
-                                    <input type="radio" name="gender" value="Female" <?= ($gender && $gender == "Female") ?
-                                                                                            'checked="checked"' : "";
-                                                                                        ?> /> Female
-                                    <input type="radio" name="gender" value="Other" <?= ($gender && $gender == "Other") ?
-                                                                                        'checked="checked"' : "";
-                                                                                    ?> /> Other
-                                                                                    <span><?= $genderError ?></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><p>Where did you born?</p></td>
-                                <td> : <input type="text" name="question1" value="<?php echo $question1 ?>"/> <span><?= $question1Error ?></span></td>
-                            </tr>
-                            <tr>
-                                <td><p>What is your birth year?</p></td>
-                                <td> : <input type="text" name="question2" value="<?php echo $question2 ?>"/> <span><?= $question2Error ?></span></td>
-                            </tr>
-                        </table>
-                        <div>
-                            <p><?= $error ?></p>
-                            <br>
-                            <input type="submit" value="Sign Up" name="submit" />
-                        </div>
-                    </form>
-                </center>
-            </td>
-        </tr>
+                    <label for="email"><b>Email: </b></label> 
+                    <input type="email" id="email" name="email" value="<?php echo $email ?>" > <span id="emailErr"></span>
+                    <?= $emailError ?> <br> <br>
+
+                    <br><hr><br>
+
+                    <label for="username"><b>Username: </b></label>
+                    <input type="text" id="username" name="username" value="<?php echo $username ?>" > <span id="usernameErr"></span>
+                    <?= $usernameError ?> <br> <br>
+
+                    <label for="password"><b>Password: </b></label>  
+                    <input type="password" id="passwoed" name="password" value="<?php echo $password ?>" > <span id="passwordErr"></span>
+                    <?= $passwordError ?> <br> <br>
+
+                    <label for="confirmPassword"><b>Confirm Password: </b></label>
+                    <input type="password" id="confirmPassword" name="confirmPassword" value="<?php echo $confirmPassword ?>" > <span id="confirmpassErr"></span>
+                    <?= $confirmPasswordError ?> <br>        
+
+                    <br><hr><br>
+
+                    <label for="ques1"><b>Where did you born? </b></label>
+                    <input type="text" id="ques1" name="question1" value="<?php echo $question1 ?>" > <span id="quetion1Err"></span> <br>
+                    <?= $question1Error ?> <br>
+
+                    <label for="ques2"><b>What is your Birthplace? </b></label>
+                    <input type="text" id="ques2" name="question2" value="<?php echo $question2 ?>" > <span id="quetion2Err"></span> <br>
+                    <?= $question2Error ?> <br>  
+
+                    <?= $error ?>
+                </div>
+                
+                <div>
+                    <input onclick="validate()" class="submit_btn" type="submit" name="submit" value="Sign Up" >
+                </div>                
+            </fieldset>
+        </form>
+        <p class="links"> <b> Already have an Account? </b> <a href='login.php'>Login Now </a></p>
+    </div>
         <tr>
             <td colspan = "2" align = "center">
                 <h6>Copyright @ 2017</h6>
