e17a1972672d1d8ceb5216903f285943f78aa0ca
mdtanvirisl <mdtanvirislam945@gmail.com>
2023-12-10 12:40:43 +0600

    css login and resitration

--- view/login.php
+++ view/login.php
@@ -9,14 +9,37 @@
 
 <head>
     <title>LOGIN</title>
+    <link rel="stylesheet" href="../assets/css/body.css">
 </head>
 
 <body>
     <?php include_once("home_menu.php");?>
-    <table border = "1" width= 100%>
+    <div class="form">
+        <form action="" method="post" enctype="" novalidate>
+            <fieldset>
+                <legend>Login: </legend>
         
-        <tr>
-            <td colspan = "2">
+                <?= $error ?> <br><br>
+                
+                <div class="input_box">
+                    User Name: <input type="text" name="username" value="" /> <br><br>
+                    Password: <input type="password" name="password" value="" /> <br><br>
+                </div>
+
+                <hr>
+
+                <input type="checkbox" name="remember_me" id="checkbox"> 
+                <label for="checkbox">Remember Me? </label> <br><br>
+                
+                <input class="submit_btn" type="submit" name="submit" value="Login" />
+            
+            </fieldset>
+        </form>
+    </div>
+        
+    <p class="links"> <b> Forget Password? </b> <a href='forgetPassword.php'> Click Here </a></p>
+    <p class="links"> <b> Don't have an Account? </b> <a href='registration.php'> Register Now </a></p>
+<!-- 
                 <form method="post" action="" enctype="">
                     <fieldset>
                         <legend>LOGIN</legend>
@@ -29,15 +52,8 @@
                         <input type="submit" name="submit" value="Submit" />
                         <a href="forgetPassword.php">Forget Password?</a>
                     </fieldset>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <td colspan = "2" align = "center">
+                </form> -->
                 <h6>Copyright @ 2017</h6>
-            </td>
-        </tr>
-    </table>
     
 </body>
 
