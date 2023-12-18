f732454e855931e1e1978a1b126519539a785481
mdtanvirisl <mdtanvirislam945@gmail.com>
2023-12-18 11:33:59 +0600

    formation check up

--- view/login.php
+++ view/login.php
@@ -39,21 +39,6 @@
         
     <p class="links"> <b> Forget Password? </b> <a href='forgetPassword.php'> Click Here </a></p>
     <p class="links"> <b> Don't have an Account? </b> <a href='registration.php'> Register Now </a></p>
-<!-- 
-                <form method="post" action="" enctype="">
-                    <fieldset>
-                        <legend>LOGIN</legend>
-                        User Name: <input type="text" name="username" value="" /> <br>
-                        Password: <input type="password" name="password" value="" /> <br>
-                        <p><?= $error ?></p>
-                        <hr>
-                        <input type="checkbox" name="remember_me" id="checkbox">
-                        <label for="checkbox"> Remember Me</label><br>
-                        <input type="submit" name="submit" value="Submit" />
-                        <a href="forgetPassword.php">Forget Password?</a>
-                    </fieldset>
-                </form> -->
-                <h6>Copyright @ 2017</h6>
     
 </body>
 
